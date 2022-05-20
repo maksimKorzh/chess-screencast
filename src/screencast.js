1a25b478e3e56ecac3db7deae447f4d7f17dea9d
Maksim Korzh <freesoft.for.people@gmail.com>
2022-05-21 02:10:07 +0300

    tested  autoplay idea

--- src/screencast.js
+++ src/screencast.js
@@ -18,26 +18,37 @@ function delay(milliseconds){
   
   // init recorder
   const recorder = new PuppeteerVideoRecorder({
-    format: 'jpeg',
+    format: 'jpg',
     quality: 100,
     maxWidth: 1920,
     maxHeight: 1080,
-    everyNthFrame: 2 // don't skip frames
+    everyNthFrame: 1 // How many frames to ignore befor taking the next screenshots
   });
   
+  // init recorder
   await recorder.init(page, '.');
   
   // navigate to game page
   await page.goto('https://www.chess.com/a/2CVzszZxSr7bL');
-  await recorder.start();
+  await delay(1000);
   
-  // capture video
-  for (let i = 0; i < 30; i++) { // move number
-    await delay(500);
+  // click on 'Details' to enable autoplay button
+  await page.evaluate(`document.querySelector('a[data-cy="analysis-tab-button-details-tab"]').click()`);
+  await delay(5000);
+  await recorder.start();
     
-    await document.querySelector('span[class="game-controls-secondary-button-icon icon-font-chess chevron-right"]').click();
+  // click on 'Play' button to start the game
+  //await page.evaluate(`document.querySelectorAll('button[class="ui_v5-button-component ui_v5-button-basic game-controls-primary-button"]')[2].click()`);
+  //await delay(25000);
+
+  // capture video
+  for (let i = 0; i < 20; i++) { // move number
+    
+      await page.evaluate(`document.querySelectorAll('button[class="ui_v5-button-component ui_v5-button-basic game-controls-primary-button"]')[3].click()`);
+    //await page.evaluate(`document.querySelector('span[class="game-controls-secondary-button-icon icon-font-chess chevron-right"]').click()`);
     //await page.keyboard.press('ArrowRight');
-    await delay(500);
+    
+    await delay(1000);
     console.log('recording move ' + (i + 1));
   }
   
@@ -45,3 +56,12 @@ function delay(milliseconds){
   await recorder.stop();
   await browser.close();
 })();
+
+
+// analysis-review-tab.chunk.client.a60d0906.js
+// 3952 ; Const De; Next.onClick
+// wpJsonpChessCom_analysis[7][1].3952
+// wpJsonpChessCom_analysis[7][1]['3952']
+
+
+
